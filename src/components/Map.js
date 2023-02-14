4b30409efbd886751cd3e2b07ada48e061fc836d
Micah Meindertsma <micahmeindertsma@Micahs-MacBook-Pro.local>
2023-02-14 14:18:35 -0500

    connect facility add for to backend

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer } from 'react-leaflet';
 import { Icon } from "leaflet";
 import { AddFacilPopup } from "../utils/mapHelper";

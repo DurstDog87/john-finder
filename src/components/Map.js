fd1761b446ccd2406b32cf0f140ce586687c03cf
Micah Meindertsma <micahmeindertsma@Micahs-MacBook-Pro.local>
2023-02-25 18:32:31 -0500

    add facility plotting on the map and give it an icon

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { Icon } from "leaflet";
 import { AddFacilPopup } from "../utils/mapHelper";
 import '../App.css';
-import '../style/map.css'
+import '../style/map.css';
+import portaIcon from "../assets/porta1.png";
+
+const porta = new Icon({
+    iconUrl: portaIcon,
+    iconRetinaUrl: portaIcon,
+    iconSize: [30, 30]
+})
 
 
 // The main map
 const Map = () => {
     const [markers, setMarkers] = useState([]);
+    const [currentJohn, setJohn] = useState(null);
+
+    function handleDelete(id) {
+        console.log(id);
+        
+        fetch(`http://localhost:5000/facilities/${id}`, {
+            method: "DELETE"
+        })
+    }
+
     const getMarkers = async () => {
         try {
             const response = await fetch("http://localhost:5000/facilities/");
@@ -36,7 +53,22 @@ const Map = () => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <AddFacilPopup />
+                {markers.map(feature => (
+                    <Marker
+                        key={feature.fid}
+                        position={[ feature.geom.x, feature.geom.y ]}
+                        icon={porta}>
+                            <Popup>
+                                <div>
+                                    <h2>{feature.condition}</h2>
+                                    <p>{feature.submitter_description}</p>
+                                    <p className="mt-3">submitted on: {feature.submitted_on}</p>
+                                </div>
+                                <button className="buttonCancel" type="button" onClick={() => handleDelete(feature.fid)}>Delete</button>
+                            </Popup>
+                    </Marker>
+                ))}
+               <AddFacilPopup />
             </MapContainer>
         </div>
     )
